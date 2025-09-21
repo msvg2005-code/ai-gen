9b72269cc7e935125322149ec2294d9113f2126b
msvg2005-code <msvg2005@gmail.com>
2025-09-21 11:04:49 +0530

    Updated DropdownSelector.js

--- components/DropdownSelector.js
+++ components/DropdownSelector.js
@@ -30,6 +30,7 @@ export default function DropdownSelector({
         <Text style={styles.label}>
           {label}
           {required && <Text style={styles.required}> *</Text>}
+          }
         </Text>
       )}
       
